e5451a5e652e19862c7cfed90c4fc2ad3ecdf4f6
Jefwillems <jefwillems100@gmail.com>
2019-08-08 15:46:13 +0200

    Trim env variables

--- BezettingsmonitorFtpUpload/api/FtpCsvWriter.cs
+++ BezettingsmonitorFtpUpload/api/FtpCsvWriter.cs
@@ -9,20 +9,24 @@ namespace BezettingsmonitorFtpUpload.api
   public static class FtpCsvWriter
   {
     public static void WriteLines(IEnumerable<string> lines) {
-      var host = Environment.GetEnvironmentVariable("FTP_HOST");
+      var host = Environment.GetEnvironmentVariable("FTP_HOST")?.Trim();
       var port = int.Parse(Environment.GetEnvironmentVariable("FTP_PORT"));
-      var username = Environment.GetEnvironmentVariable("FTP_USERNAME");
-      var password = Environment.GetEnvironmentVariable("FTP_PASSWORD");
+      var username = Environment.GetEnvironmentVariable("FTP_USERNAME")?.Trim();
+      var password = Environment.GetEnvironmentVariable("FTP_PASSWORD")?.Trim();
 
       var connectionInfo =
-        new ConnectionInfo(host?.Trim(), port, username?.Trim(),
-          new PasswordAuthenticationMethod(username?.Trim(), password?.Trim()));
+        new ConnectionInfo(host, port, username, new PasswordAuthenticationMethod(username, password));
+      
       using (var sftp = new SftpClient(connectionInfo)) {
         sftp.Connect();
-        var text = string.Join("\n", lines);
-        var buffer = Encoding.UTF8.GetBytes(text);
-        sftp.UploadFile(new MemoryStream(buffer), "flexwhere-meetings.csv", true);
-        sftp.Disconnect();
+        
+        if (sftp.IsConnected) {
+          Console.WriteLine("Connected to sftp.");
+          var text = string.Join("\n", lines);
+          var buffer = Encoding.UTF8.GetBytes(text);
+          sftp.UploadFile(new MemoryStream(buffer), "/data/flexwhere-meetings.csv", true);
+          sftp.Disconnect();
+        }
       }
 
       Console.WriteLine("Writing to sftp was successful.");
